5595ced0df2896e124b64cec60560e9b24fec28f
Ahmad Alsaleh <aalsalh474@gmail.com>
2025-05-21 17:50:23 +0300

    Update me.js

--- controllers/me.js
+++ controllers/me.js
@@ -24,8 +24,14 @@ const patchUserProfile = async (req, res) => {
 };
 
 const deleteUserProfile = async (req, res) => {
+  const userId = req.userId;
   try {
-    res.status(200).json('not implemented yet Profile deleted');
+    const result = await pool.query(
+      'DELETE FROM users WHERE id = $1', 
+      [userId]
+    );
+
+    res.status(200).json(`users deleted`);
   } catch (err) {
     res.status(500).json({ 
       error: err.message });
@@ -48,8 +54,15 @@ const getUserAds = async (req, res) => {
 };
 
 const postAd = async (req, res) => {
+  const userId = req.userId;
+  const {title, description, price, image_path, category_id , city_id  } = req.body;
   try {
-    res.status(200).json('not implemented yet post Ad');
+    console.log(userId)
+    const result = await pool.query(
+      'INSERT INTO ads (user_id, title, description, price, image_path, category_id , city_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, title, price',
+      [userId,title, description, Number(price), image_path, Number(category_id) , Number(city_id)]
+    );
+    res.status(201).json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ 
       error: err.message });
