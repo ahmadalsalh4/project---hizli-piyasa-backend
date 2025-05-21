af0ae8cbe2b2ee378c8157f6a624c341255b9bbe
Ahmad Alsaleh <aalsalh474@gmail.com>
2025-05-22 02:11:50 +0300

    Update imageUploader.js

--- services/imageUploader.js
+++ services/imageUploader.js
@@ -2,13 +2,11 @@ const axios = require('axios');
 
 const uploadToImgBB = async (base64) => {
     try {
-        const formData = new URLSearchParams();
-        formData.append('key', process.env.IMGBB_API_KEY);
-        formData.append('image', base64);
 
+        const formDataString = `key=${encodeURIComponent(process.env.IMGBB_API_KEY)}&image=${encodeURIComponent(base64)}`;
         const response = await axios.post(
             'https://api.imgbb.com/1/upload',
-            formData.toString(),
+            formDataString,
             {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
