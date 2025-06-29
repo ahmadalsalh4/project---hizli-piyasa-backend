6359582a5281cbb2dc0371316c38403df3c976e7
Ahmad Alsaleh <aalsalh474@gmail.com>
2025-06-30 02:27:01 +0300

    a

--- routes/me.js
+++ routes/me.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const container = require("../services/container");
-const authController = require("../controllers/me")(container);
+const meController = require("../controllers/me")(container);
 const auth = container.get("auth");
 
-router.get("/", auth, authController.getUserProfile);
-router.patch("/", auth, authController.patchUserProfile);
-router.delete("/", auth, authController.deleteUserProfile);
+router.get("/", auth, meController.getUserProfile);
+router.patch("/", auth, meController.patchUserProfile);
+router.delete("/", auth, meController.deleteUserProfile);
 
-router.get("/ads", auth, authController.getUserAds);
-router.post("/ads", auth, authController.postAd);
-router.get("/ads/:id", auth, authController.getUserAdDetailed);
-router.patch("/ads/:id", auth, authController.patchAd);
-router.delete("/ads/:id", auth, authController.deleteAd);
+router.get("/ads", auth, meController.getUserAds);
+router.post("/ads", auth, meController.postAd);
+router.get("/ads/:id", auth, meController.getUserAdDetailed);
+router.patch("/ads/:id", auth, meController.patchAd);
+router.delete("/ads/:id", auth, meController.deleteAd);
 
 module.exports = router;
